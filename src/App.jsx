00a313a03a109958d08d778d07b04d618724ccad
Hernandes <hernandeshass910@gmail.com>
2025-04-11 01:21:10 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Cabecalho } from './componentes/'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello Word</h1>
+      <Cabecalho />
+    </>
+  )
+}
+
+export { App }
