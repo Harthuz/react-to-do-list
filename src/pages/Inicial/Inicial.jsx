947a566df3f447bd19d163da22d16d4d9550d51a
Hernandes <hernandeshass910@gmail.com>
2025-04-11 03:36:42 -0300

    feat: update Inicial component styling and integrate FormularioTarefas with proper layout

--- src/pages/Inicial/Inicial.jsx
+++ src/pages/Inicial/Inicial.jsx
@@ -1,8 +1,9 @@
 import { FormularioTarefas } from "../../componentes"
+import style from "./Inicial.module.css"
 
 const Inicial = () => {
     return (
-        <div>
+        <div className={style.Inicial}>
             <h1>Bem-vindo à página inicial!</h1>
             <p>Esta é a página inicial do nosso aplicativo.</p>
             <FormularioTarefas />
