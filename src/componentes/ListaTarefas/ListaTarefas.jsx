ad4382516551f0a09a2b66030dad812fbba42e27
Hernandes <hernandeshass910@gmail.com>
2025-04-14 21:24:16 -0300

    feat: refactor ListaTarefas to use a dynamic tarefas array for rendering items

--- src/componentes/ListaTarefas/ListaTarefas.jsx
+++ src/componentes/ListaTarefas/ListaTarefas.jsx
@@ -2,11 +2,16 @@ import { ItemListaTarefas } from "../../componentes"
 import style from "./ListaTarefas.module.css"
 
 const ListaTarefas = () => {
+    const tarefas = [
+        { id: 1, nome: "Tarefa 1" },
+        { id: 2, nome: "Tarefa 2" },
+        { id: 3, nome: "Tarefa 3" },
+    ]
     return (
         <ul className={style.ListaTarefas}>
-        <ItemListaTarefas nome="Tarefa 1" />
-        <ItemListaTarefas nome="Tarefa 2" />
-        <ItemListaTarefas nome="Tarefa 3" />
+        {tarefas.map(tarefa => (
+            <ItemListaTarefas key={tarefa.id} nome={tarefa.nome} />
+        ))}
         </ul>
     )
 }
